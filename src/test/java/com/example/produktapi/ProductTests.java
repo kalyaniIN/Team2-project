f061ca8a214f248346852f52667bede662da3bb2
Uma <umalearning2023@gmail.com>
2023-12-06 08:09:11 +0100

    Added test_checkProductId

--- src/test/java/com/example/produktapi/ProductTests.java
+++ src/test/java/com/example/produktapi/ProductTests.java
@@ -1,6 +1,8 @@
 package com.example.produktapi;
 
 import com.example.produktapi.model.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProductTests {
@@ -12,4 +14,19 @@ public void checkProductTitle(){
     System.out.println(a.getTitle());
 
 }
+
+@Test
+    void checkProductId(){
+    Product p1= new Product();
+    Product p2= new Product();
+
+    p1.setId(1001);
+    p2.setId(-777);
+
+    Assertions.assertEquals(1001,p1.getId());
+    Assertions.assertEquals(-777,p2.getId());
+
+
+}
+
 }
