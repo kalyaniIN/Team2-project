60c12178dc44d4053d100b21eec591358f1688e5
Suzana <suzana.ivko@gmail.com>
2023-12-07 17:32:06 +0100

    Added 3 testcases.

--- src/test/java/com/example/produktapi/ProductTests.java
+++ src/test/java/com/example/produktapi/ProductTests.java
@@ -1,15 +1,28 @@
 package com.example.produktapi;
 
 import com.example.produktapi.model.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ProductTests {
 
-    Product a =new Product("SchoolBag",123.222,"Bag","Adidas","img.jpg");
-@Test
-public void checkProductTitle(){
-
-    System.out.println(a.getTitle());
+    Product product =new Product("Bracelet",123.222,"Jewelery","Silver dragon bracelet","img.jpg");//
+    // Suzana. Testing Get Description. Positive testing.
+    @Test
+public void checkProductDescription(){
+    Assertions.assertEquals("Silver dragon bracelet", product.getDescription());
 
 }
+    // Suzana. Testing Get Description. Negative testing.
+    @Test
+    public void checkProductDescription2(){
+        Assertions.assertEquals("123456", product.getDescription());
+
+    }
+///Suzana. Testing setting a new Description of a Product.
+    @Test
+    public void AddNewProductDescription() {
+    product.setDescription("A new bracelet");
+    Assertions.assertEquals("A new bracelet", product.getDescription());
+    }
 }
