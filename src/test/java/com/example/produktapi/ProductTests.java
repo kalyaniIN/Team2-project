626cfb846267c3de91718b1ba9e3cd286495542f
Uma <umalearning2023@gmail.com>
2023-12-11 16:43:44 +0100

    Added testcases and changed folder structure

--- src/test/java/com/example/produktapi/ProductTests.java
+++ src/test/java/com/example/produktapi/ProductTests.java
@@ -1,9 +1,8 @@
 package com.example.produktapi;
-
 import com.example.produktapi.model.Product;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ProductTests {
@@ -11,15 +10,13 @@ Product product =new Product("SchoolBag",123.222,"Bag","Adidas","img.jpg");
     // Suzana. Testing Get Description. Positive testing.
     
     @Test
-public void checkProductDescription(){
+    public void checkProductDescription(){
     Assertions.assertEquals("Adidas", product.getDescription());
-
-}
+    }
     // Suzana. Testing Get Description. Negative testing.
     @Test
     public void checkProductDescription2(){
-        Assertions.assertNotEquals("123456", product.getDescription());
-
+    Assertions.assertNotEquals("123456", product.getDescription());
     }
 //Suzana. Testing setting a new Description of a Product.
     @Test
@@ -28,8 +25,6 @@ public void checkProductDescription(){
     Assertions.assertEquals("A new bracelet", product.getDescription());
     }
 
-
-    //Product product =new Product("SchoolBag",123.222,"Bag","Adidas","img.jpg");
     @Test
     public void checkProductCategory(){
         //Get the category.
@@ -37,6 +32,7 @@ public void checkProductDescription(){
         // check the category is correct.
         Assertions.assertEquals("Bag", productCategory);
     }
+
     @Test
     public void setNewCategoryAndCheckThatNewCategoryRetuns(){
         //Declare and set a new category to an existing category.
@@ -47,6 +43,7 @@ public void checkProductDescription(){
         // check the category is correct.
         Assertions.assertEquals(newCategory, productCategory);
     }
+
     @Test
     public void checkProductPrice(){
         //Get the price.
@@ -55,22 +52,16 @@ public void checkProductDescription(){
         Assertions.assertEquals(123.222, productPrice);
     }
 
-
-}
-
-@Test
+    @Test
     void checkProductId(){
-    Product products= new Product();
+        Product products= new Product();
     
-    products.setId(1001);
-    Assertions.assertEquals(1001,products.getId());
+        products.setId(1001);
+        Assertions.assertEquals(1001,products.getId());
     
-    products.setId(-777);
-    Assertions.assertEquals(-777,products.getId());
-
-
-}
-
+        products.setId(-777);
+        Assertions.assertEquals(-777,products.getId());
+    }
 
     @Test
     public void setNewPriceAndCheckThatNewPriceReturns(){
@@ -83,5 +74,42 @@ public void checkProductDescription(){
         Assertions.assertEquals(newPrice, productPrice);
     }
 
+    @Test
+    public void testGetTitle() {
+        // Use the getTitle method and assert the result
+        String productTitle = product.getTitle();
+        assertEquals("SchoolBag", productTitle);
+    }
+
+    @Test
+    public void testSetTitle() {
+        // Set a new title using the setTitle method
+        String newTitle = "UnicornBag";
+        product.setTitle(newTitle);
+
+        String productTitle = product.getTitle();
+
+        // Use the getTitle method to get the updated title and assert the result
+        assertEquals("UnicornBag", productTitle);
+    }
+
+    @Test
+    public void testGetImage() {
+        String productImage = product.getImage();
+        // Use the getImage method and assert the result
+        assertEquals("img.jpg", productImage);
+    }
+
+    @Test
+    public void testSetImage() {
+        // Set a new image using the setImage method
+        String newImage = "new-image.jpg";
+        product.setImage(newImage);
+
+        String productImage = product.getImage();
+
+        // Use the getImage method to get the updated image and assert the result
+        assertEquals("new-image.jpg", productImage);
+    }
 
 }
