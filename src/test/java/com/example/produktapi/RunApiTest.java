1ba06fd1a7dcaa0f4c450bac9bad4ce8b7b0618b
Uma <umalearning2023@gmail.com>
2023-12-12 17:16:06 +0100

    Added testcases and changed folder structure_Integrationtesting

---
+++ src/test/java/com/example/produktapi/RunApiTest.java
@@ -0,0 +1,14 @@
+package com.example.produktapi;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/feature/")
+
+public class RunApiTest {
+
+
+
+}
