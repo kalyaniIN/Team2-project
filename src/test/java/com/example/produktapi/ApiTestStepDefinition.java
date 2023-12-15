b7cbce9677065bb068e327ff49b248adeda50c18
Uma <umalearning2023@gmail.com>
2023-12-15 17:08:57 +0100

    Added integration testcases for /my-endpont and /product

--- src/test/java/com/example/produktapi/ApiTestStepDefinition.java
+++ src/test/java/com/example/produktapi/ApiTestStepDefinition.java
@@ -1,12 +1,11 @@
 package com.example.produktapi;
-
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
-
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.*;
 
@@ -16,19 +15,39 @@ public class ApiTestStepDefinition {
 
     private static Response response;
 
-    @When("Given url")
+    @When("Given endpointurl")
     public void getEndPoint() {
+
         baseURI="https://produktapi-6ef53ba8f2f2.herokuapp.com/my-endpoint";
         request = given();
         response =request.request(Method.GET,"");
     }
     @Then("Status for http request is {int}")
     public void checkEndPoint(Integer expectedcode) {
+
         int actualcode =response.getStatusCode();
         Assertions.assertEquals(expectedcode,actualcode);
+    }
+
+    @Then("ContentType is text")
+    public void body_is() {
+
+        response.then().assertThat().contentType(ContentType.TEXT);
+
 
     }
 
+    @When("Given producturl")
+    public void given_producturl() {
+        baseURI="https://produktapi-6ef53ba8f2f2.herokuapp.com/products";
+        request = given();
+        response =request.request(Method.GET,"");
+    }
+
+    @Then("contentType is JSON")
+    public void content_type_is_json() {
+        response.then().assertThat().contentType(ContentType.JSON);
+    }
 
 
 }
--- src/test/resources/Feature/Apitestcases.feature
+++ src/test/resources/Feature/Apitestcases.feature
@@ -1,5 +1,20 @@
 Feature: Api testing
 
-  Scenario:TEst /my-endpoint
-    When Given url
+  Scenario:Test /my-endpoint
+    When Given endpointurl
     Then Status for http request is 200
+
+  Scenario: Verify the contentType of /endpoint
+    When Given endpointurl
+    Then ContentType is text
+
+  Scenario:Test /products
+    When Given producturl
+    Then Status for http request is 200
+
+  Scenario: Verify the contentType of /products
+    When Given producturl
+    Then contentType is JSON
+
+
+
