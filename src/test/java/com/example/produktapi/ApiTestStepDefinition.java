1995bdd8f361138deb4fad40e50619a97b4bf02b
kalyaniIN <61795426+kalyaniIN@users.noreply.github.com>
2023-12-15 17:00:55 +0100

    Added integration test cases for product categories

--- src/test/java/com/example/produktapi/ApiTestStepDefinition.java
+++ src/test/java/com/example/produktapi/ApiTestStepDefinition.java
@@ -1,5 +1,6 @@
 package com.example.produktapi;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.Method;
@@ -28,6 +29,22 @@ public class ApiTestStepDefinition {
         Assertions.assertEquals(expectedcode,actualcode);
 
     }
+    @Given("the application is running")
+    public void the_application_is_running() {
+
+    }
+    @When("the client requests GET \\/products\\/categories")
+    public void the_client_requests_get_products_categories() {
+        baseURI="https://produktapi-6ef53ba8f2f2.herokuapp.com/products/categories";
+        request = given();
+        response = request.request(Method.GET, "");
+    }
+    @Then("the response status should be {int}")
+    public void the_response_status_should_be(Integer expectedStatus) {
+        int actualStatus = response.getStatusCode();
+        Assertions.assertEquals(expectedStatus,actualStatus);
+    }
+
 
 
 
--- src/test/resources/Feature/Apitestcases.feature
+++ src/test/resources/Feature/Apitestcases.feature
@@ -3,3 +3,8 @@ Feature: Api testing
   Scenario:TEst /my-endpoint
     When Given url
     Then Status for http request is 200
+
+  Scenario: Retrieve all categories
+    Given the application is running
+    When the client requests GET /products/categories
+    Then the response status should be 200
