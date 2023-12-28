5c05e15488e73b4ccf972f26bd011b7b3bdcb44f
Faisal Farman <faisal.farman@aspia.se>
2023-12-28 16:07:42 +0100

    Added testcase goToElectronicsAndCheckNumberOfProduct() and addElectronicsProductAndCheckThatItIsAddedToTheCheckOut()

--- src/test/java/com/example/produktapi/SeleniumTestCases.java
+++ src/test/java/com/example/produktapi/SeleniumTestCases.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,6 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class SeleniumTestCases {
@@ -90,6 +90,68 @@ public class SeleniumTestCases {
         //verify the number of products displayed in jewelery page
         List<WebElement> items= driver.findElements(By.className("col"));
         Assertions.assertEquals(4,items.size());
+    }
+
+    @Test
+    void goToElectronicsAndCheckNumberOfProduct(){
+        //Go to shop
+        driver.findElement(By.linkText("Shop")).click();
+        //wait
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //Go to Electronics
+        driver.findElement(By.linkText("Electronics")).click();
+
+        //wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        //verify the number of products displayed in jewelery page
+        int numberOfElectronicsProduct = driver.findElements(By.className("col")).size();
+        Assertions.assertEquals(6,numberOfElectronicsProduct);
+
+    }
+
+    @Test
+    void addElectronicsProductAndCheckThatItIsAddedToTheCheckOut(){
+        //Go to Shop
+        driver.findElement(By.linkText("Shop")).click();
+        //wait
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //Go to Electronics page
+        driver.findElement(By.linkText("Electronics")).click();
+
+        //Save the number of the product in the Cart for later assert.
+        var gettingNumberOfTheProductInTheCart = driver.findElement(By.xpath("//*[@id=\"buttonSize\"]")).getText();
+
+        int numberOfTheProductInTheCart;
+        //Check if the cart was empty or not, if empty then setting value to 0.
+        if(Objects.equals(gettingNumberOfTheProductInTheCart, "")) {
+            numberOfTheProductInTheCart = 0;
+        }
+        else {
+            //If the cart is not empty then parsing the string value to integer.
+            numberOfTheProductInTheCart = Integer.parseInt(gettingNumberOfTheProductInTheCart);
+        }
+        //increasing the product number by 1 for later assert and saving in a variable.
+        int newNumberOfTheProductInTheCart = numberOfTheProductInTheCart + 1;
+
+        //wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        //Add a product to cart
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div/div/button")).click();
+        //Getting number of product to the cart after adding a new product
+        var gettingNumberOfTheProductAfterAddingANew = driver.findElement(By.xpath("//*[@id=\"buttonSize\"]")).getText();
+        //Parsing the value from string to Int.
+        int numberOfTheProductInTheCartAfterAddingANew = Integer.parseInt(gettingNumberOfTheProductAfterAddingANew);
+        // Verify that the product is added to the checkout.
+        Assertions.assertEquals(newNumberOfTheProductInTheCart,numberOfTheProductInTheCartAfterAddingANew );
     }
 
     @AfterAll
--- src/test/resources/Feature/SystemTests.feature
+++ src/test/resources/Feature/SystemTests.feature
@@ -5,7 +5,6 @@ Feature: SystemTesting
     When the user visits the website
     Then the title should be "Webbutiken"
 
-
   Scenario: Test the number of products displayed when clicking on Jewelery link
     Given website is available
     When the user visits the website
@@ -23,3 +22,22 @@ Feature: SystemTesting
     When user click on women's clothing
     When wait for 1000 millisec
     Then verify the number of product is 6
+
+  Scenario: Test the number of products displayed when clicking on Electronics link
+    Given website is available
+    When the user visits the website
+    When user clicks on Shop
+    When wait for 1000 millisec
+    When user click on electronics
+    Then verify the number of product is 6
+
+
+  Scenario: Add a electronics product to the cart
+    Given website is available
+    When the user visits the website
+    When user clicks on Shop
+    When wait for 1000 millisec
+    When user click on electronics
+    When Get the number of product in the checkout
+    When User click on Add to cart
+    When Get the number of product in the checkout after adding a product
